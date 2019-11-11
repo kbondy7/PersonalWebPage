cf40b5e3c03ad558c6d992d216e740473b78530b
Kieran Bondy <k.bondmna7@gmail.com>
2019-11-11 16:27:42 +0100

    yh

--- HomePage_Personal.js
+++ HomePage_Personal.js
@@ -10,21 +10,21 @@ function Hide(id) {
 
 function goTo(id) {
   if(id == "K") {
-    window.location.href = "file:///Users/kieranbondy/Desktop/NORTHWESTERN/website/HomePage_Personal.html";
+    window.location.href = "https://sad-galileo-416040.netlify.com/index.html";
   }
   if(id == "I") {
     window.location.href = "https://sad-galileo-416040.netlify.com/InterestsPage_Personal.html";
   }
   if(id == "E") {
-    window.location.href = "file:///Users/kieranbondy/Desktop/NORTHWESTERN/website/ExperiencePage_Personal.html";
+    window.location.href = "https://sad-galileo-416040.netlify.com//ExperiencePage_Personal.html";
   }
   if(id == "R") {
-    window.location.href = "file:///Users/kieranbondy/Desktop/NORTHWESTERN/website/ResumePage_Personal.html";
+    window.location.href = "https://sad-galileo-416040.netlify.com//ResumePage_Personal.html";
   }
   if(id == "A") {
-    window.location.href = "file:///Users/kieranbondy/Desktop/NORTHWESTERN/website/AbilitiesPage_Personal.html";
+    window.location.href = "https://sad-galileo-416040.netlify.com/AbilitiesPage_Personal.html";
   }
   if(id == "N") {
-    window.location.href = "file:///Users/kieranbondy/Desktop/NORTHWESTERN/website/NotableProjectPage_Personal.html";
+    window.location.href = "https://sad-galileo-416040.netlify.com/NotableProjectPage_Personal.html";
   }
 }
